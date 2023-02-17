d0cfb1bbe13ce4d87592891a79c96bd88346113c
Sumant <24x7sumant@gmail.com>
2023-02-17 17:31:19 +0400

    commented and finished code to get desired output

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 const express =  require('express')
 const app = express()
 const https = require('https');
-app.use(express.static("public"));
+app.use(express.static("public")); 
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs") //using ejs for the web app
 
 app.get('/', (req, res) => {
     const url = 'https://api.openweathermap.org/data/2.5/weather?q=Dubai&appid=4df1bce68ef8ae00b6c3c865096d3824&units=metric'
-    https.get(url, (response) => {
+    https.get(url, (response) => { //getting the url of the weather api and storing the response in response
         //console.log(response)
-        response.on('data', (data) => {
+        response.on('data', (data) => { 
             const weatherData = JSON.parse(data);
             const CurrentTemp = weatherData.main.temp;
             const description = weatherData.weather[0].description;
-            //console.log(description);
-            //res.send("The temperature currently is : " + CurrentTemp + " degrees celsius")
+            res.render('index', { CurrentTemp, description }); // renders the index.ejs file while sending the CurrentTemp and description values over.
         })
     })
-    res.render('index') // renders the index.ejs file
+    
 })
 
-app.listen(4001, () => console.log("Server started on port 4001")) // listening on port 4000 
+app.listen(4005, () => console.log("Server started on port 4005")) // listening on port 4000 
 
 
